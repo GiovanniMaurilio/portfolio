e5bd3b4e2118e252f80b2ff2233b5f3426d18e20
GiovanniMaurilio <giovannimaurilio556@gmail.com>
2026-03-19 09:00:48 +0100

    email

--- da_portfolio-main/public/sendMail.php
+++ da_portfolio-main/public/sendMail.php
@@ -27,7 +27,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $headers[] = 'Content-type: text/html; charset=utf-8';
 
             // Additional headers
-            $headers[] = "From: noreply@portfolio.mgiesi.at";
+            $headers[] = "From: giovannimaurilio04@gmail.com";
 
             mail($recipient, $subject, $message, implode("\r\n", $headers));
             break;
